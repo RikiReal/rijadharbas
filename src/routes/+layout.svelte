4b17691b36ddff5a2dffd485694e7ba33bd475b4
RikiReal <rijad@tutanota.de>
2024-02-26 15:29:48 +0100

    Add tailwindcss and set adapter static

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+	import '../app.css';
+</script>
+
+<header>
+	<nav aria-labelledby="primary-navigation">
+		<a href="/">
+			<img src="" alt="Logo" />
+		</a>
+		<ul id="primary-navigation">
+			<li><a href="#about-me">About Me</a></li>
+			<li><a href="#services">Services</a></li>
+			<li><a href="#contact-me">Contact</a></li>
+		</ul>
+	</nav>
+</header>
+
+<slot />
+
+<footer>
+	<nav>
+		<a href="legal-notice.html">Legal Notice</a>
+		<a href="data-privacy.html">Data Privacy</a>
+	</nav>
+</footer>
