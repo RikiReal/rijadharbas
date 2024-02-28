5691942c7faa5954613b8d5c2d23168cfe0d9224
RikiReal <rijad@tutanota.de>
2024-02-28 12:15:00 +0100

    Navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,23 +2,52 @@
 	import '../app.css';
 </script>
 
-<header>
-	<nav aria-labelledby="primary-navigation">
-		<a href="/">
-			<img src="" alt="Logo" />
-		</a>
-		<ul id="primary-navigation">
-			<li><a href="#about-me">About Me</a></li>
-			<li><a href="#services">Services</a></li>
-			<li><a href="#contact-me">Contact</a></li>
-		</ul>
+<header class="sticky top-0 z-50 bg-transparent">
+	<nav aria-labelledby="primary-navigation" class="navbar absolute">
+		<div class="navbar-start">
+			<a href="/" class="flex-none">
+				<img src="" alt="Logo" />
+			</a>
+		</div>
+
+		<div class="navbar-end">
+			<div class="dropdown dropdown-end">
+				<div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h8m-8 6h16"
+						/></svg
+					>
+				</div>
+				<ul
+					class="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow"
+				>
+					<li><a href="#about-me">About Me</a></li>
+					<li><a href="#services">Services</a></li>
+					<li><a href="#contact-me">Contact</a></li>
+				</ul>
+			</div>
+			<ul id="primary-navigation" class="menu menu-horizontal hidden lg:flex">
+				<li><a href="#about-me">About Me</a></li>
+				<li><a href="#services">Services</a></li>
+				<li><a href="#contact-me">Contact</a></li>
+			</ul>
+		</div>
 	</nav>
 </header>
 
 <slot />
 
 <footer>
-	<nav>
+	<nav class="footer">
 		<a href="legal-notice.html">Legal Notice</a>
 		<a href="data-privacy.html">Data Privacy</a>
 	</nav>
